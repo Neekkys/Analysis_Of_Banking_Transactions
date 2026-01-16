6ba50f5f8e9c2e9ec4a3c3eda81874bb1de94c3d
Nikita Kovetskiy <nikita.koveckij@gmail.com>
2026-01-17 02:57:14 +0300

    Ready category web pages

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,29 @@
+import json
+import os
 from datetime import datetime
 from pathlib import Path
-from typing import List, Any, Dict
+from typing import Any, Dict, List, Tuple, Union
 
 import pandas as pd
+import requests
+from dotenv import load_dotenv
 from pandas import DataFrame
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+def get_xlsx_path() -> str:
+    data_dir = BASE_DIR.joinpath("data")
+    if not data_dir.exists():
+        return "Файла не существует"
+    excel_extensions = [".xlsx", ".xls", ".xlsm", ".xlsb"]
+    try:
+        for item in data_dir.iterdir():
+            if item.is_file() and item.suffix.lower() in excel_extensions:
+                return item.name
+        return "error"
+    except Exception as ex:
+        return f"error {ex}"
 
 
 def time_for_greeting() -> str:
@@ -22,56 +40,206 @@ def time_for_greeting() -> str:
         return "Доброй ночи"
 
 
-def get_time_period(date_time: str, date_format: str = "%Y-%m-%d %H:%M:%S") -> List[str]:
+def get_time_period(date_time: str, date_format: str = "%Y-%m-%d %H:%M:%S") -> List[str] | str:
     """Функция принимает на вход вторую границу периода даты и возвращает
     1 границу периода - начало месяца, и 2 границу периода в виде списка"""
-    dt = datetime.strptime(date_time, date_format)
-    first_day = dt.replace(day=1, hour=0, minute=0, microsecond=0)
-    return [
-        first_day.strftime("%d.%m.%Y %H:%M:%S"),
-        dt.strftime("%d.%m.%Y %H:%M:%S"),
-    ]
+    try:
+        dt = datetime.strptime(date_time, date_format)
+        first_day = dt.replace(day=1, hour=0, minute=0, microsecond=0)
+        return [
+            first_day.strftime("%d.%m.%Y %H:%M:%S"),
+            dt.strftime("%d.%m.%Y %H:%M:%S"),
+        ]
+    except Exception as ex:
+        return f"error {ex}"
 
 
-def slice_period_and_sort_df(data_path: Path, period: List[str]) -> DataFrame:
+def slice_period_and_sort_df(data_path: Path, period: List[str] | str) -> DataFrame | str:
     """Функция принимает путь к Excel файлу и период, состоящий из 2 дат.
     Функция читает xlsx, обрезает по указанному периоду(инд 0 - начало месяца, инд 1 - до какого дня)
     и преобразует xlsx файл в виде словаря"""
-    df = pd.read_excel(data_path, sheet_name="Отчет по операциям")
-    df["Номер карты"] = df["Номер карты"].fillna("SPB pay or transfers between accounts")
-    df["Дата операции"] = pd.to_datetime(df["Дата операции"], dayfirst=True)
+    try:
+        if isinstance(period, str):
+            return "Ошибка"
+        df = pd.read_excel(data_path, sheet_name="Отчет по операциям")
+        df["Номер карты"] = df["Номер карты"].fillna("SPB pay or transfers between accounts")
+        df["Дата операции"] = pd.to_datetime(df["Дата операции"], dayfirst=True)
 
-    start_time = datetime.strptime(period[0], "%d.%m.%Y %H:%M:%S")
-    last_time = datetime.strptime(period[1], "%d.%m.%Y %H:%M:%S")
+        start_time = datetime.strptime(period[0], "%d.%m.%Y %H:%M:%S")
+        last_time = datetime.strptime(period[1], "%d.%m.%Y %H:%M:%S")
 
-    filtered_df = df[(df["Дата операции"] >= start_time) & (df["Дата операции"] <= last_time)]
+        filtered_df = df[(df["Дата операции"] >= start_time) & (df["Дата операции"] <= last_time)]
 
-    sorted_df = filtered_df.sort_values(by="Дата операции")
-    return sorted_df
+        sorted_df = filtered_df.sort_values(by="Дата операции")
+        return sorted_df
+    except Exception as ex:
+        return f"error {ex}"
 
 
-def spending_on_the_card(sorted_df: DataFrame) -> List[Dict[str, Any]]:
+def spending_on_the_card(sorted_df: DataFrame | str) -> List[Dict[str, Any]] | str:
     """Функция принимает отсортированный по дате и периоду DataFrame и извлекает из него:
     last_digits - последние 4 цифры номера карты,
     total_spent - общая сумма расходов по карте,
     cashback - общая сумма кешбека"""
-    transactions_for_card = []
-    expenses = sorted_df[sorted_df["Сумма операции"] < 0]
-    sorted_by_group = expenses.groupby("Номер карты")
+    if isinstance(sorted_df, str):
+        return []
+    try:
+        df = sorted_df.copy()
+        transactions_for_card = []
+        expenses = df[df["Сумма операции"] < 0]
+        sorted_by_group = expenses.groupby("Номер карты")
 
-    for card_number, group in sorted_by_group:
-        card_number = str(card_number)
-        if "SPB" not in card_number and len(card_number) >= 4:
-            last_digits = str(card_number[-4:])
+        for card_number, group in sorted_by_group:
+            card_number = str(card_number)
+            if "SPB" not in card_number and len(card_number) >= 4:
+                last_digits = str(card_number[-4:])
+            else:
+                last_digits = str(card_number)
+            total_spent = abs(group["Сумма операции"].sum())
+            cashback = group["Кэшбэк"].fillna(0).sum()
+            transactions_for_card.append(
+                {
+                    "last_digits": last_digits,
+                    "total_spent": round(total_spent, 2),
+                    "cashback": cashback,
+                }
+            )
+        return transactions_for_card
+    except Exception as ex:
+        return f"error {ex}"
+
+
+def get_top_transactions(sorted_df: DataFrame | str, count: int = 5) -> List[Dict[str, Any]] | str:
+    """Функция принимает отсортированный по периоду даты датафрейм и ищет топ (сount) транзакций
+    по сумме платежа. Затем выводит в виде словаря:
+    "date": Дата
+    "amount": Сумма операции
+    "category": Категория
+    "description": Описание"""
+    if isinstance(sorted_df, str):
+        return []
+    try:
+        result = []
+        df = sorted_df.copy()
+        df["Сумма операции"] = df["Сумма операции"].abs()
+        top_transactions = df.sort_values(by="Сумма операции", ascending=False).head(count)
+        for transactions in top_transactions.to_dict("records"):
+            date_time_timestamp = transactions.get("Дата платежа")
+            if date_time_timestamp is None:
+                date_time = "Не указана"
+            else:
+                try:
+                    date_time = date_time_timestamp.strftime("%d.%m.%Y")
+                except AttributeError:
+                    # Если это не timestamp, пытаемся преобразовать в строку
+                    date_time = str(date_time_timestamp)
+
+            amount = transactions.get("Сумма операции")
+            category_ = transactions.get("Категория", "None category")
+            description = transactions.get("Описание", "None description")
+            result.append(
+                {
+                    "date": date_time,
+                    "amount": amount,
+                    "category": category_,
+                    "description": description,
+                }
+            )
+        return result
+    except Exception as ex:
+        return f"error {ex}"
+
+
+def open_json() -> Union[Any, str]:
+    """Функция распаковки json файла. Возвращает пример:
+    {
+      "user_currencies": ["USD", "EUR"],
+      "user_stocks": ["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"]
+    }"""
+    try:
+        data_dir = BASE_DIR.joinpath("user_settings.json")
+        with open(data_dir, "r", encoding="utf8") as f:
+            unpacked_json = json.load(f)
+        return unpacked_json
+    except Exception as ex:
+        return f"error {ex}"
+
+
+load_dotenv()
+api_key_currency = os.getenv("API_KEY_CURRENCIES")
+api_url_currency: str = os.getenv("API_URL_CURRENCIES", "")
+
+
+def currency_api(opened_json: Union[Dict[str, Any], str]) -> Tuple[List[Dict[str, Any]], float] | str:
+    """Функция запрашивает у внешнего API курс валют, указанных в распакованном json файле.
+     Возвращает список словарей с указанными:
+    {"currency": "Валюта", "rate": "Стоимость"}"""
+    if isinstance(opened_json, str):
+        return [], 0
+    try:
+        user_currencies = opened_json.get("user_currencies", "Ошибка")
+        if user_currencies == "Ошибка":
+            currency_value = user_currencies
         else:
-            last_digits = str(card_number)
-        total_spent = abs(group["Сумма операции"].sum())
-        cashback = group["Кэшбэк"].fillna(0).sum()
-        transactions_for_card.append(
-            {
-                "last_digits": last_digits,
-                "total_spent": round(total_spent, 2),
-                "cashback": cashback,
-            }
-        )
-    return transactions_for_card
+            currency_value = ",".join(user_currencies)
+        payload = {"base": "RUB", "symbols": currency_value}
+        headers = {"apikey": api_key_currency}
+
+        response = requests.request("GET", api_url_currency, headers=headers, params=payload)
+        response.raise_for_status()
+        json_response = response.json()
+
+        result_list_dict = []
+        real_price_usd = 0
+
+        for value, count in json_response["rates"].items():
+            if count == 0:
+                continue
+            rate = round(1 / count, 2)
+            if value == "USD":
+                real_price_usd = rate
+            result_list_dict.append(
+                {
+                    "currency": value,
+                    "rate": rate,
+                }
+            )
+        return result_list_dict, real_price_usd
+    except requests.exceptions.RequestException as e:
+        return f"Ошибка запроса к API: {e}"
+    except (KeyError, ValueError, ZeroDivisionError, TypeError) as e:
+        return f"Ошибка обработки данных: {e}"
+
+
+load_dotenv()
+api_key_stocks = os.getenv("API_KEY_STOCKS")
+
+
+def current_stock_prise(opened_json: Union[Dict[str, Any], str], real_price_usd: float) -> List[Dict[str, Any]] | str:
+    """Функция запрашивает у внешнего API курс акций, указанных в распакованном json файле.
+    Возвращает список словарей с указанными:
+    {"stock": "Акция", "price": "Стоимость"}"""
+    if isinstance(opened_json, str):
+        return []
+    try:
+        user_stocks = opened_json.get("user_stocks", [])
+
+        result_list_dict = []
+
+        for stock in user_stocks:
+            url = f"https://api.finage.co.uk/last/stock/{stock}?apikey={api_key_stocks}"
+
+            response = requests.request("GET", url)
+            response.raise_for_status()
+            response_json = response.json()
+            stock = response_json.get("symbol")
+            price_usd = response_json.get("ask")
+            price_rub = round(float(price_usd) * float(real_price_usd), 2)
+
+            result = {"stock": stock, "price": price_rub}
+            result_list_dict.append(result)
+        return result_list_dict
+    except (ValueError, KeyError, TypeError) as ex:
+        return f"Ошибка обработки данных: {ex}"
+    except requests.exceptions.RequestException as ex:
+        return f"Ошибка запроса к API: {ex}"
--- src/views.py
+++ src/views.py
@@ -1,31 +1,133 @@
 import json
+import logging
+from typing import Any, Dict, List
 
-from src.utils import BASE_DIR, get_time_period, slice_period_and_sort_df, spending_on_the_card, time_for_greeting
+from src.utils import (
+    BASE_DIR,
+    currency_api,
+    current_stock_prise,
+    get_time_period,
+    get_top_transactions,
+    get_xlsx_path,
+    open_json,
+    slice_period_and_sort_df,
+    spending_on_the_card,
+    time_for_greeting,
+)
+
+logger = logging.getLogger("views.py")
+logger.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler()
+stream_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+stream_handler.setFormatter(stream_formatter)
+logger.addHandler(stream_handler)
 
 
-def main_response(date_time: str) -> str:
+def main_response(date_time: str) -> str | dict:
     """Главная функция, принимающая на вход строку с датой и временем в формате
     YYYY-MM-DD HH:MM:SS и возвращающую JSON-ответ
     Пример: 2025-04-20 16:20:00"""
 
-    # 1. Принимаем путь. Тут будет функция автоматического считывания файлов Excel.
+    logger.info("Начало работы")
+    # 1. Функция автоматического считывания названия файла Excel.
     # Здороваемся в зависимости от времени суток
-    data_dir = BASE_DIR.joinpath("data", "operations.xlsx")
+
+    logger.info("Старт get_xlsx_path()")
+    excel_name = get_xlsx_path()
+    if excel_name == "Файла не существует":
+        logger.warning("Файла не существует")
+    elif "error" in excel_name:
+        logger.error(f"Фундаментальная ошибка - {excel_name}")
+    else:
+        logger.info("Выполнено")
+
+    data_dir = BASE_DIR.joinpath("data", excel_name)
+
+    logger.info("Старт time_for_greeting()")
     greeting = time_for_greeting()
+    if (
+        greeting == "Добрый день"
+        or greeting == "Добрый вечер"
+        or greeting == "Доброе утро"
+        or greeting == "Доброй ночи"
+    ):
+        logger.info("Выполнено")
+    else:
+        logger.error("Ошибка")
 
     # 2.1. Находим период дат (от начала месяца до указанной границы date_time)
     # и сортируем датафрейм по указанному периоду дат
+    logger.info("Старт get_time_period(date_time)")
     time_period = get_time_period(date_time)
+    if isinstance(time_period, str) and "error" in time_period:
+        logger.error(f"Фундаментальная ошибка {time_period} c датой ({date_time})")
+    else:
+        logger.info("Выполнено")
+
+    logger.info("Старт slice_period_and_sort_df(data_dir, time_period)")
     sorted_df = slice_period_and_sort_df(data_dir, time_period)
+    if isinstance(sorted_df, str) or "error" in sorted_df:
+        logger.error(f"Ошибка - {sorted_df}")
+    else:
+        logger.info("Выполнено")
 
     # 2.2. Извлекает из датафрейма данные о транзакциях по карте и возвращает в виде списка словарей
+    logger.info("Старт spending_on_the_card(sorted_df)")
     cards = spending_on_the_card(sorted_df)
+    if isinstance(cards, str) or "error" in cards:
+        logger.error(f"Ошибка - {cards}")
+    else:
+        logger.info("Выполнено")
 
+    # 3. Возвращает топ 5 транзакций по сумме платежа. Вторым аргументом можно передать int (цифра топа)
+    logger.info("Старт get_top_transactions(sorted_df)")
+    top_transactions = get_top_transactions(sorted_df)
+    if isinstance(top_transactions, str) or "error" in top_transactions:
+        logger.error(f"Ошибка - {top_transactions}")
+    else:
+        logger.info("Выполнено")
+
+    # 4.1 Читаем json с данными о необходимых курсах валют и акций
+    logger.info("Старт open_json()")
+    opened_json = open_json()
+    if isinstance(opened_json, str) or "error" in opened_json:
+        logger.error(f"Ошибка - {opened_json}")
+    else:
+        logger.info("Выполнено")
+
+    # 4.2 Делаем запрос к API для получения актуальных курсов валют и актуальной стоимости USD для
+    # функции stock_prices
+    logger.info("Старт currency_api(opened_json)")
+    success_currency_api = currency_api(opened_json)
+    currency_rates: List[Dict[str, Any]] = []
+    real_price_usd: float = 0.0
+    if isinstance(success_currency_api, str):
+        logger.error(f"Ошибка в currency_api: {success_currency_api}")
+    else:
+        currency_rates, real_price_usd = success_currency_api
+        logger.info("Выполнено")
+
+    # 4.3 Делаем запрос к API для получения актуальных курсов акций
+    logger.info("Старт current_stock_prise(opened_json, real_price_usd)")
+    stock_prices = current_stock_prise(opened_json, real_price_usd)
+    if isinstance(stock_prices, str) or "Ошибка" in stock_prices:
+        logger.error(f"Ошибка в current_stock_prise: {stock_prices}")
+    else:
+        logger.info("Выполнено")
+
+    logger.info("Формирование словаря с финальным ответом в виде JSON")
     data = {
         "greeting": greeting,
         "cards": cards,
+        "top_transactions": top_transactions,
+        "currency_rates": currency_rates,
+        "stock_prices": stock_prices,
     }
 
-    to_json_data = json.dumps(data, ensure_ascii=False, indent=4)
-
-    return to_json_data
+    try:
+        to_json_data = json.dumps(data, ensure_ascii=False, indent=4)
+        logger.info("Выполнено")
+        return to_json_data
+    except Exception as ex:
+        logger.error(f"Ошибка {ex}")
+        return {}
